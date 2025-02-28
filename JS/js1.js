f32ea64a3c6c53651797ce7f5446c67c5cea6795
Al Shahariar Arafat Shawon <shahariararafat7739@gmail.com>
2025-02-28 12:28:32 +0600

    login page done

---
+++ JS/js1.js
@@ -0,0 +1,8 @@
+let input1=document.querySelector('#input1');
+let input2=document.querySelector('#input2');
+
+input1.addEventListener("focus", function()
+{
+    input1.style.color="#42A5F5"
+    // input1.placeholder=''
+})
